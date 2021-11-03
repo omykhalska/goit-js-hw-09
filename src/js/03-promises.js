39074aa848788f2b9ca4fedd8b27be0f8dbf3806
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-11-03 22:23:46 +0200

    Добавляет решение задания №3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,57 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delayField: document.querySelector('input[name="delay"]'),
+  stepField: document.querySelector('input[name="step"]'),
+  amountField: document.querySelector('input[name="amount"]'),
+  buttonEl: document.querySelector('button[type="submit"]'),
+};
+
+refs.buttonEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(e) {
+  e.preventDefault();
+
+  const { delayField, stepField, amountField } = refs;
+  if (!isFormFulfilled(delayField, stepField, amountField)) {
+    return;
   }
+
+  const amount = +amountField.value;
+  const step = +stepField.value;
+  let delay = +delayField.value;
+
+  for (let index = 0; index < amount; index++) {
+    const position = index + 1;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function isFormFulfilled(input1, input2, input3) {
+  if (!input1.value || !input2.value || !input3.value) {
+    Notify.failure(`Error: all fields must be filled in !`);
+    return false;
+  }
+  return true;
 }
